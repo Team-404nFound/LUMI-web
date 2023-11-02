303b25bc5aad8bc69e5f31610ee699cda80c4c3e
BruceBest <BruceLvJC@outlook.com>
2023-11-02 15:20:55 -0400

    got animation. need close button

---
+++ source/JS/main.js
@@ -0,0 +1,34 @@
+item=document.getElementsByClassName('team')
+for(i=0;i<item.length;i++) {
+    item[i].addEventListener('mouseenter', listener);
+}
+
+function listener(e){
+    preview=document.getElementById('preview');
+    document.getElementById("frame").src="peoples/preview/"+
+        e.toElement.getAttribute("href").substring(8);
+    preview.style.display = 'block';
+    console.log(document.getElementById("frame").src);
+    document.getElementById('team').style="transform:translateX(-10em);";
+    for(j=0;j<1;j+=0.1){
+        setTimeout(function(){preview.style="opacity:"+j+";"},10);
+    }
+}
+
+function closePreview(){
+    preview=document.getElementById('preview')
+    document.getElementById('team').style="transform:translateX(0);"
+    for(j=1;j>=0;j-=0.1){
+        setTimeout(function(){preview.style="opacity:"+j+";"},5)
+    }
+    setTimeout(function(){preview.style.display = 'none';},2000)
+}
+
+
+// item=document.getElementsByClassName('team')
+// for(i=0;i<item.length;i++) {
+//     item[i].addEventListener('mouseleave', function() {
+//         setTimeout(function(){document.getElementById('preview').style.display = 'none';
+//         document.getElementById('team').style="transform:translateX(0);"}, 30)
+//     });
+// }
